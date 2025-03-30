f1f526dd2491b6029aaba3295b3641bbb2266951
Advay Singh <153810091+firefoundary@users.noreply.github.com>
2025-03-30 17:43:03 +0530

    Update App.tsx

--- Desktop/project/src/App.tsx
+++ Desktop/project/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GameBoard from './components/Mathgame/GameBoard';
-import Header from './components/Header';
+import Header from './components/Mathgame/Header';
 
 function App() {
   return (
